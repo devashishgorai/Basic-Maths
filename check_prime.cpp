0f3563fd5f5de4de0055f3d4f69f443f93db99af
Devashish Gorai <devashishgorai10@gmail.com>
2026-01-07 10:54:33 +0530

    Added check prime number file

---
+++ check_prime.cpp
@@ -0,0 +1,30 @@
+// Problem Name : Check Prime Number
+// Description  : Determines whether a given number is prime
+// Approach     : Trial division up to sqrt(n)
+// Time Complexity : O(√n)
+// Author       : Devashish Gorai
+
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n) {
+    if (n <= 1) return false;
+
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    if (isPrime(n))
+        cout << "true";
+    else
+        cout << "false";
+
+    return 0;
+}
